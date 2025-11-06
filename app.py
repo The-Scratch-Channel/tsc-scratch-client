a99c7a3b77306e12f32083c6fea00c9df3d76cd6
Graison P <119984713+Graison-P@users.noreply.github.com>
2025-11-06 10:15:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import scratchattach as sa
-from scratchattach import Encoding
-scratch_username = "username"
-scratch_password = "password"
-cloud = session.connect_cloud("1149116249")
-cloud.set_var("Title", 'Encoding.encode("Welcome!")')
-cloud.set_var("Contents", 'Encoding.encode("This project is a place where scratch news is shared. Stay tuned for more info!")')
-cloud = session.connect_tw_cloud("1149116249")
-cloud.set_var("Title", 'Encoding.encode("Welcome!")')
-cloud.set_var("Contents", 'Encoding.encode("This project is a place where scratch news is shared. Stay tuned for more info!")')
+# import scratchattach as sa
+# from scratchattach import Encoding
+# scratch_username = "username"
+# scratch_password = "password"
+# cloud = session.connect_cloud("1149116249")
+# cloud.set_var("Title", 'Encoding.encode("Welcome!")')
+# cloud.set_var("Contents", 'Encoding.encode("This project is a place where scratch news is shared. Stay tuned for more info!")')
+# cloud = session.connect_tw_cloud("1149116249")
+# cloud.set_var("Title", 'Encoding.encode("Welcome!")')
+# cloud.set_var("Contents", 'Encoding.encode("This project is a place where scratch news is shared. Stay tuned for more info!")')
